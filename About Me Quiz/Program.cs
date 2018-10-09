590781726fea44c59e9830ea7b9d2a0aae6914f0
Allisa LeBeuf <al5906@gmail.com>
2018-10-09 10:55:50 -0700

    question one and question two created

--- About Me Quiz/Program.cs
+++ About Me Quiz/Program.cs
@@ -7,6 +7,45 @@ namespace About_Me_Quiz
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome, please take a quiz about me!");
+
+            Console.WriteLine("What state am I from?");
+            string questionOne = Console.ReadLine();
+
+            Console.WriteLine("Guess the day of my birth");
+            int questionTwo = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine($"You answered {questionOne}. Your answer is: {QuestionOne(questionOne)}");
+            QuestionTwo(questionTwo);
+
+
+
+        }
+        //Question 1: Method to check users input with an if statement
+        static string QuestionOne(string input)
+        {
+            if (input == "CO" || input == "Colorado")
+            {
+                return "Correct";
+            }
+            else
+                return "Incorrect";
+        }
+
+        //Question 2: Method to check if user's integar input is correct
+        static int QuestionTwo(int numInput)
+        {
+            if (numInput == 29)
+            {
+                Console.WriteLine("Your answer is correct.");
+                return 29;
+            }
+            else
+            {
+                Console.WriteLine($"That is incorrect. You are {29-numInput} days off");
+                return 29-numInput;
+            }
         }
     }
 }
